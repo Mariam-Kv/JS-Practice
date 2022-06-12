b8430717a834e035e52e4c57a92585c09d780d35
Mari Kvirkviya <marikanik1999@gmail.com>
2022-06-12 10:07:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function func(){
+    let a = prompt("How old are you?");
+    let b=(a*365);
+    let h1 = document.createElement("h1");
+    let text=document.createTextNode("You are "+ b +" days old.");
+   h1.appendChild(text);
+   h1.setAttribute("id","wow");
+   document.getElementById("js").appendChild(h1)
+}
+function func2(){
+    
+   document.getElementById("wow").remove();}
+
+
+
+function adddogs(){
+    let img = document.createElement("img");
+    let dog = document.getElementById("js2")
+    img.src="/challenges/chill-bro.gif"
+
+    dog.appendChild(img)
+
+}
