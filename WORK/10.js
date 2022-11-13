cdfce1f0a8fb7debaaf281e3d1858e1ad9802ad0
Mashawabupr <marikanik1999@gmail.com>
2022-11-13 13:33:32 +0300

    done

---
+++ WORK/10.js
@@ -0,0 +1,38 @@
+let x = "1113222113";
+let x2 = [];
+let index = 0;
+let counter = 0;
+let str = "";
+x = x.split("");
+let step = 0;
+while (step <= 40) {
+  
+  for (let i of x) {
+    if (index === 0 && x[index] !== x[index + 1]) {
+      str += `1${x[index]}`;
+      index += 1;
+    }
+    if (index !== 0 && x[index - 1] !== x[index] && x[index] !== x[index + 1]) {
+      str += `1${x[index]}`;
+      index += 1;
+      counter = 0;
+    }
+    if (x[index] === x[index + 1]) {
+      counter += 1;
+      index += 1;
+      if (x[index] !== x[index + 1]) {
+        counter += 1;
+        str += `${counter}${x[index]}`;
+        counter = 0;
+        index += 1;
+      }
+    }
+  }
+  console.log( str.split(""));
+  index = 0;
+  counter = 0;
+  step += 1;
+  x = str.split("");
+  str = "";
+}
+
--- WORK/4.js
+++ WORK/4.js
@@ -1,8 +1,41 @@
 let total = 0;
 let counter = 0;
-fetch("04.txt")
+let city = "Лондон";
+let arr1 = [];
+let arr2 = [];
+let arr3 = [];
+let arr4 = [];
+let arr5 = [];
+let arr6 = [];
+let arr7 = [];
+fetch("011.txt")
   .then((response) => response.text())
   .then((text) => {
-    let data = text.split(", ");
-    console.log(data);
+    let array = [];
+    let obj = { 1: [] };
+    let data = text.split("\n");
+
+    for (let i of data) {
+      i = i.split(" ");
+      if (i.includes(city)) {
+        arr1.push(i);
+      }
+      array.push(i);
+    }
+    for (let i of array) {
+      if (i.includes(city)) {
+        arr2.push(i);
+      }
+    }
+
+    for (let i of array) {
+      for (let a of arr2) {
+        if (i[0] === a[1] && a[1] !== i[1]) {
+          a.push(i);
+        }
+      }
+      console.log(arr2);
+    }
+
+    //console.log(array);
   });
