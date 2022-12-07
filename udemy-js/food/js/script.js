c856622f05c93cc1cbcedbc979f4ab02c7283656
Mashawabupr <marikanik1999@gmail.com>
2022-12-07 21:34:05 +0300

    added some functionality

--- udemy-js/food/js/script.js
+++ udemy-js/food/js/script.js
@@ -1,5 +1,91 @@
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".tabheader__items").addEventListener("click", (e) => {
-    console.log(e.target.classList.add("tabheader__item_active"));
+  let tabs = document.querySelectorAll(".tabheader__item");
+  let tabParent = document.querySelector(".tabheader__items");
+  let contents = document.querySelectorAll(".tabcontent");
+  function hide() {
+    tabs.forEach((el) => {
+      el.classList.remove("tabheader__item_active");
+    });
+    contents.forEach((el) => {
+      el.style.display = "none";
+    });
+  }
+  function show(id = 0) {
+    tabs[id].classList.add("tabheader__item_active");
+    contents[id].style.display = "block";
+  }
+
+  hide();
+  show();
+  tabParent.addEventListener("click", (e) => {
+    if (e?.target?.classList.contains("tabheader__item")) {
+      tabs.forEach((el, i) => {
+        if (e.target === el) {
+          hide();
+          show(i);
+        }
+      });
+    }
+  });
+  //timer
+  let deadLine = "2023-01-01";
+  function getTimeRemaining(endTime) {
+    let time = Date.parse(endTime) - Date.parse(new Date());
+
+    let days = Math.floor(time / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    let minutes = Math.floor((time / (1000 * 60)) % 60);
+    let sec = Math.floor((time / 1000) % 60);
+    return {
+      time,
+      days,
+      hours,
+      minutes,
+      sec,
+    };
+  }
+  function setOclock(timer, endtime) {
+    let timerBlock = document.querySelector(timer);
+    let t = getTimeRemaining(endtime);
+    timerBlock.querySelector("#days").innerHTML = getZero(t.days);
+    timerBlock.querySelector("#hours").innerHTML = getZero(t.hours);
+    timerBlock.querySelector("#minutes").innerHTML = getZero(t.minutes);
+    timerBlock.querySelector("#seconds").innerHTML = getZero(t.sec);
+  }
+
+  let interval = setInterval(() => {
+    setOclock(".timer", deadLine);
+  }, 1000);
+  if (Date.parse(deadLine) - Date.parse(new Date()) < 0) {
+    clearTimeout(interval);
+  }
+  function getZero(num) {
+    if (num < 10) {
+      return `0${num}`;
+    }
+    return num;
+  }
+  //modal
+  let btns = document.querySelectorAll(".btn");
+  let modal = document.querySelector(".modal");
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+  btns.forEach((el) => {
+    el.addEventListener("click", () => {
+      modal.style.display = "block";
+      document.body.style.overflow = "hidden";
+    });
+  });
+  document.querySelector(".modal__close").addEventListener("click", () => {
+    modal.style.display = "none";
+    document.body.style.overflow = "scroll";
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.style.display === "block") {
+      modal.style.display = "none";
+    }
   });
 });
