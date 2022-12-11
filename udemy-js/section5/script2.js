8a67696b4251132b0ef5ddbd385b1364575f1199
Mashawabupr <marikanik1999@gmail.com>
2022-12-11 20:40:30 +0300

    finish

--- udemy-js/section5/script2.js
+++ udemy-js/section5/script2.js
@@ -1,1 +1,30 @@
-console.log(3);
+import { tns } from "./node_modules/tiny-slider/src/tiny-slider";
+tns({
+  container: ".my-slider",
+  items: 1,
+  slideBy: "page",
+  autoplay: true,
+});
+// function* generator() {
+//   yield 3;
+//   yield "three";
+// }
+// let gen = generator();
+// console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
+// function* count(n) {
+//   for (let i = 0; i < n; i++) {
+//     yield i;
+//   }
+// }
+// let counter = count(5);
+// console.log(counter.next().value);
+// console.log(counter.next());
+// console.log(counter.next());
+// console.log(counter.next());
+// console.log(counter.next());
+// console.log(counter.next());
+// for (let k of count(3)) {
+//   console.log(k);
+// }
